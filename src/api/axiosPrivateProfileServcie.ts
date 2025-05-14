0eeacd9d9ec21476273ca9c5b623451149e48646
Nguyễn Huy Phúc <huyphuc280503@gmail.com>
2025-05-15 01:27:50 +0700

    refactor: remove redundant response interceptor logic and clean up error handling

--- src/api/axiosPrivateProfileServcie.ts
+++ src/api/axiosPrivateProfileServcie.ts
@@ -47,46 +47,6 @@ axiosPrivateProfileServcie.interceptors.response.use(
 
     return Promise.reject(error);
   },
-  (config) => {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-      config.headers = config.headers || {};
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
-
-axiosPrivateProfileServcie.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      try {
-        const refreshResponse = await axios.post<RefreshResponse>(
-          'http://localhost:8088/api/v1/auth/refresh',
-          {},
-          { withCredentials: true },
-        );
-
-        const newToken = refreshResponse.data.data.token;
-        localStorage.setItem('accessToken', newToken);
-        //console.log("newToken: "+newToken);
-        originalRequest.headers.Authorization = `Bearer ${newToken}`;
-        return axiosPrivateProfileServcie(originalRequest);
-      } catch (refreshError) {
-        localStorage.removeItem('accessToken');
-        window.location.href = '/login';
-        return Promise.reject(refreshError);
-      }
-    }
-
-    return Promise.reject(error);
-  },
 );
 
 export default axiosPrivateProfileServcie;
