dd075ef0674ce79b3b29eeb3e8a3d7995bf35073
Nguyễn Huy Phúc <huyphuc280503@gmail.com>
2025-05-20 22:20:56 +0700

    Refactor StudentProfile and StudentVerification components for improved readability and functionality
    - Updated import statements for consistency in quotation marks.
    - Added loading state to StudentVerification component for better user experience.
    - Enhanced error handling and user notifications in both components.
    - Improved image upload handling and display logic in StudentVerification.
    - Cleaned up code formatting and structure for better maintainability.

--- src/pages/identity/user/StudentProfile.tsx
+++ src/pages/identity/user/StudentProfile.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
-import "./UserProfile.css";
-import { StudentProfileService } from "../../../services/user/StudentProfile/StudentProfileService";
-import { StudentProfile as StudentProfileModel } from "../../../services/user/StudentProfile/StudentProfile";
+import React, { useEffect, useState } from 'react';
+import './UserProfile.css';
+import { StudentProfileService } from '../../../services/user/StudentProfile/StudentProfileService';
+import { StudentProfile as StudentProfileModel } from '../../../services/user/StudentProfile/StudentProfile';
 import { RequestStudents } from '../../../services/user/StudentProfile/StudentProfileService';
-import { StudentVerifycationService } from "../../../services/user/StudentVerifycation/StudentVerifycationService";
+import { StudentVerifycationService } from '../../../services/user/StudentVerifycation/StudentVerifycationService';
 import { useNavigate } from 'react-router-dom';
-import Swal from "sweetalert2";
-import { StudentCard } from "../../../services/user/StudentProfile/StudentCard";
+import Swal from 'sweetalert2';
+import { StudentCard } from '../../../services/user/StudentProfile/StudentCard';
+import Loading from '../../../common/Loading';
 export default function StudentProfile() {
   const navigate = useNavigate();
 
@@ -16,21 +17,19 @@ export default function StudentProfile() {
   const [selectedImage, setSelectedImage] = useState(String);
 
   useEffect(() => {
-
     //localStorage.clear();
 
     const fetchProfile = async () => {
       try {
         const service = new StudentProfileService();
         const data = await service.getStudentProfile();
-        console.log("newToken: " + localStorage.getItem('accessToken'));
+        console.log('newToken: ' + localStorage.getItem('accessToken'));
         console.log(data);
         setSelectedImages(
           data?.getStudentCardDTOS()?.map((item: StudentCard) => ({
-            url: item.getStudentCardUrl()
-          })) || []
+            url: item.getStudentCardUrl(),
+          })) || [],
         );
-
 
         if (!data) {
           handleNotifycation();
@@ -38,7 +37,7 @@ export default function StudentProfile() {
         }
         setProfile(data);
       } catch (error) {
-        console.error("Failed to fetch student profile:", error);
+        console.error('Failed to fetch student profile:', error);
       }
     };
 
@@ -46,10 +45,10 @@ export default function StudentProfile() {
       try {
         const resquest = new StudentProfileService();
         const data = await resquest.getRequestStudent();
-        console.log("sang" + data);
+        console.log('sang' + data);
         setRequest(data);
       } catch (error) {
-        console.error("Failed to fetch student profile:", error);
+        console.error('Failed to fetch student profile:', error);
       }
     };
     fetchSendRequest();
@@ -66,19 +65,18 @@ export default function StudentProfile() {
       allowOutsideClick: false,
     }).then((result) => {
       if (result.isConfirmed) {
-        navigate('/studentverifycation')
+        navigate('/studentverifycation');
       }
     });
   };
 
   const handleEditProfile = () => {
     navigate('/updateprofile');
-  }
-
+  };
 
   const handleSendRequest = () => {
     Swal.fire({
-      text: "Are you send request student profile",
+      text: 'Are you send request student profile',
       icon: 'question',
       confirmButtonColor: '#28a745',
       showCancelButton: true,
@@ -119,11 +117,17 @@ export default function StudentProfile() {
     });
   };
 
+  if (!profile) {
+    return <Loading />;
+  }
 
   return (
     <div className="user-profile">
       {/* Title Header */}
-      <section className="inner-header-title" style={{ backgroundImage: 'url(/assets/img/banner-10.jpg)' }}>
+      <section
+        className="inner-header-title"
+        style={{ backgroundImage: 'url(/assets/img/banner-10.jpg)' }}
+      >
         <div className="container">
           <h1>Profile Student</h1>
         </div>
@@ -133,11 +137,18 @@ export default function StudentProfile() {
       <section className="detail-desc advance-detail-pr gray-bg">
         <div className="container white-shadow">
           <div className="row">
-            <div className="detail-pic" style={{ width: '240px', height: '240px' }}>
-              <img style={{ width: '220px', height: '220px' }} src={profile?.getAvatarUrl()} alt="Candidate" />
+            <div
+              className="detail-pic"
+              style={{ width: '240px', height: '240px' }}
+            >
+              <img
+                style={{ width: '220px', height: '220px' }}
+                src={profile?.getAvatarUrl()}
+                alt="Candidate"
+              />
             </div>
             <div className="detail-status">
-              <span>{(request) ? (request?.getStatus()) : ("not send")}</span>
+              <span>{request ? request?.getStatus() : 'not send'}</span>
             </div>
           </div>
 
@@ -152,19 +163,35 @@ export default function StudentProfile() {
           <div className="row no-padd">
             <div className="detail pannel-footer">
               <div className="col-md-5 col-sm-5">
-                <ul className="detail-footer-social">
-                </ul>
+                <ul className="detail-footer-social"></ul>
               </div>
 
-              <div className="col-md-12 col-sm-12" >
+              <div className="col-md-12 col-sm-12">
                 <div className="detail-pannel-footer-btn pull-right">
-                  {(request?.getStatus() === "reject" || !request) ? (<a onClick={handleEditProfile} className="footer-btn grn-btn">Edit Now</a>) : ("")}
+                  {request?.getStatus() === 'reject' || !request ? (
+                    <a
+                      onClick={handleEditProfile}
+                      className="footer-btn grn-btn"
+                    >
+                      Edit Now
+                    </a>
+                  ) : (
+                    ''
+                  )}
                 </div>
                 <div className="detail-pannel-footer-btn pull-left">
-                  {(request?.getStatus() === "reject" || !request) ? (<a onClick={handleSendRequest} className="footer-btn blu-btn">Send Request</a>) : ("")}
+                  {request?.getStatus() === 'reject' || !request ? (
+                    <a
+                      onClick={handleSendRequest}
+                      className="footer-btn blu-btn"
+                    >
+                      Send Request
+                    </a>
+                  ) : (
+                    ''
+                  )}
                 </div>
               </div>
-
             </div>
           </div>
         </div>
@@ -175,79 +202,119 @@ export default function StudentProfile() {
         <div className="container">
           <div className="deatil-tab-employ tool-tab">
             <div>
-              <table style={{ margin: 'auto', width: '700px', }}>
+              <table style={{ margin: 'auto', width: '700px' }}>
                 <tr>
-                  <td style={{ fontSize: '20px', color: '#444444' }}>Date Of Birth:</td><td style={{ fontSize: '20px', color: 'gray' }}>{profile?.getDateOfBirth()}</td>
+                  <td style={{ fontSize: '20px', color: '#444444' }}>
+                    Date Of Birth:
+                  </td>
+                  <td style={{ fontSize: '20px', color: 'gray' }}>
+                    {profile?.getDateOfBirth()}
+                  </td>
                 </tr>
                 <tr>
-                  <td style={{ fontSize: '20px', color: '#444444' }}>Address:</td><td style={{ fontSize: '20px', color: 'gray' }}>{profile?.getAddress()}</td>
+                  <td style={{ fontSize: '20px', color: '#444444' }}>
+                    Address:
+                  </td>
+                  <td style={{ fontSize: '20px', color: 'gray' }}>
+                    {profile?.getAddress()}
+                  </td>
                 </tr>
                 <tr>
-                  <td style={{ fontSize: '20px', color: '#444444' }}>University:</td><td style={{ fontSize: '20px', color: 'gray' }}>{profile?.getUniversity()}</td>
+                  <td style={{ fontSize: '20px', color: '#444444' }}>
+                    University:
+                  </td>
+                  <td style={{ fontSize: '20px', color: 'gray' }}>
+                    {profile?.getUniversity()}
+                  </td>
                 </tr>
                 <tr>
-                  <td style={{ fontSize: '20px', color: '#444444' }}>Major:</td><td style={{ fontSize: '20px', color: 'gray' }}>{profile?.getMajor()}</td>
+                  <td style={{ fontSize: '20px', color: '#444444' }}>Major:</td>
+                  <td style={{ fontSize: '20px', color: 'gray' }}>
+                    {profile?.getMajor()}
+                  </td>
                 </tr>
                 <tr>
-                  <td style={{ fontSize: '20px', color: '#444444' }}>Phone Number:</td><td style={{ fontSize: '20px', color: 'gray' }}>{profile?.getPhoneNumber()}</td>
+                  <td style={{ fontSize: '20px', color: '#444444' }}>
+                    Phone Number:
+                  </td>
+                  <td style={{ fontSize: '20px', color: 'gray' }}>
+                    {profile?.getPhoneNumber()}
+                  </td>
                 </tr>
                 <tr>
-                  <td style={{ fontSize: '20px', color: '#444444' }}>Academic Year Start:</td><td style={{ fontSize: '20px', color: 'gray' }}>{profile?.getAcademicYearStart()}</td>
+                  <td style={{ fontSize: '20px', color: '#444444' }}>
+                    Academic Year Start:
+                  </td>
+                  <td style={{ fontSize: '20px', color: 'gray' }}>
+                    {profile?.getAcademicYearStart()}
+                  </td>
                 </tr>
                 <tr>
-                  <td style={{ fontSize: '20px', color: '#444444' }}>Academic Year End:</td><td style={{ fontSize: '20px', color: 'gray' }}>{(() => {
-                    const rawDate = profile?.getAcademicYearEnd();
-                    const endDate = rawDate ? rawDate : null;
+                  <td style={{ fontSize: '20px', color: '#444444' }}>
+                    Academic Year End:
+                  </td>
+                  <td style={{ fontSize: '20px', color: 'gray' }}>
+                    {(() => {
+                      const rawDate = profile?.getAcademicYearEnd();
+                      const endDate = rawDate ? rawDate : null;
 
-                    // Kiểm tra nếu là Invalid Date hoặc null thì in ra "not graduated"
-                    return endDate
-                      ? endDate
-                      : "not graduated";
-                  })()}</td>
+                      // Kiểm tra nếu là Invalid Date hoặc null thì in ra "not graduated"
+                      return endDate ? endDate : 'not graduated';
+                    })()}
+                  </td>
                 </tr>
                 <tr>
-                  <td style={{ fontSize: '20px', color: '#444444' }}>Image Stduent Card:</td>
+                  <td style={{ fontSize: '20px', color: '#444444' }}>
+                    Image Stduent Card:
+                  </td>
                 </tr>
               </table>
 
-              <div className="image-upload-container" style={{
-                    display: 'flex',
-                    flexDirection: 'row', // đảm bảo các items nằm ngang
-                    alignItems: 'center',
-                    gap: '10px', // khoảng cách giữa các ảnh
-                    flexWrap: 'nowrap', // ngăn không cho wrap xuống dòng
-                    overflowX: 'auto', // cho phép scroll ngang nếu nhiều ảnh
-                    padding: '10px 0',
-                    margin: 'auto',
-                    justifyContent: 'center'
-                  }}>
-                    {selectedImages.map((image, index) => (
-                      <div key={index} className="image-preview" style={{
-                        position: 'relative',
-                        minWidth: '100px', // đảm bảo kích thước tối thiểu
+              <div
+                className="image-upload-container"
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row', // đảm bảo các items nằm ngang
+                  alignItems: 'center',
+                  gap: '10px', // khoảng cách giữa các ảnh
+                  flexWrap: 'nowrap', // ngăn không cho wrap xuống dòng
+                  overflowX: 'auto', // cho phép scroll ngang nếu nhiều ảnh
+                  padding: '10px 0',
+                  margin: 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {selectedImages.map((image, index) => (
+                  <div
+                    key={index}
+                    className="image-preview"
+                    style={{
+                      position: 'relative',
+                      minWidth: '100px', // đảm bảo kích thước tối thiểu
+                      height: '100px',
+                      flexShrink: 0, // ngăn không cho ảnh co lại
+                    }}
+                  >
+                    <img
+                      src={image.url}
+                      alt={`Preview ${index}`}
+                      onClick={() => setSelectedImage(image.url)}
+                      style={{
+                        width: '100px',
                         height: '100px',
-                        flexShrink: 0 // ngăn không cho ảnh co lại
-                      }}>
-                        <img
-                          src={image.url}
-                          alt={`Preview ${index}`}
-                          onClick={() => setSelectedImage(image.url)}
-                          style={{
-                            width: '100px',
-                            height: '100px',
-                            objectFit: 'cover',
-                            borderRadius: '4px'
-                          }}
-                        />
-                      </div>
-                    ))}
+                        objectFit: 'cover',
+                        borderRadius: '4px',
+                      }}
+                    />
                   </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-        {selectedImage !== "" && (
+        {selectedImage !== '' && (
           <div
-            onClick={() => setSelectedImage("")}
+            onClick={() => setSelectedImage('')}
             style={{
               position: 'fixed',
               top: 0,
@@ -273,7 +340,6 @@ export default function StudentProfile() {
             />
           </div>
         )}
-
       </section>
     </div>
   );
